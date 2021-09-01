d7767658a01b55ab992195bac8e099f99176856e
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-09-01 09:11:29 +0100

    feat: distance cal

--- src/distance.js
+++ src/distance.js
@@ -1,4 +1,4 @@
-const distance = (lat1, lon1, lat2, lon2) => {
+export const calulateDistance = (lat1, lon1, lat2, lon2) => {
 	try {
 		if (lat1 === lat2 && lon1 === lon2) {
 			return 0;
@@ -8,17 +8,16 @@ const distance = (lat1, lon1, lat2, lon2) => {
 			let theta = lon1 - lon2;
 			let radtheta = (Math.PI * theta) / 180;
 
-			let dist = Math.acos(
+			let distance = Math.acos(
 				Math.sin(radlat1) * Math.sin(radlat2) +
 					Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta)
 			);
-			dist = (dist * 180) / Math.PI;
-			dist = dist * 60 * 1.1515;
-			dist = dist * 1.609344;
-			return dist;
+			distance = (distance * 180) / Math.PI;
+			distance = distance * 60 * 1.1515;
+			distance = distance * 1.609344;
+			return distance;
 		}
 	} catch (ex) {
-		console.log(ex);
 		return 0;
 	}
 };
